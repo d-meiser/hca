919a6c9770f3dd424bc726fcf278e66d6c444e9f
Dominic Meiser <dmeiser79@gmail.com>
2017-07-14 20:55:47 -0600

    A bit of fooling around with ensembles.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
 module Lib
     ( someFunc
+    , makeEmptyEnsemble
     ) where
+
+import qualified Data.Array.Repa as R
+import qualified Data.Map.Strict as Map
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+newtype Positions = Positions (R.Array R.U R.DIM2 Double)
+    deriving Show
+
+newtype Velocities = Velocities (R.Array R.U R.DIM2 Double)
+    deriving Show
+
+newtype ParameterName = ParameterName String
+    deriving Show
+data ParameterValue = EnsembleProperty (R.Array R.U R.DIM1 Double)
+                    | ParticleProperty Double
+    deriving Show
+newtype Parameters = Parameters (Map.Map ParameterName ParameterValue)
+    deriving Show
+
+data Ensemble = Ensemble Positions Velocities Parameters
+              | EmptyEnsemble
+    deriving Show
+
+makeEmptyEnsemble = Empty
+
+
