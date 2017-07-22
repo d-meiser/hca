4485a702935a9f9a614aa03f80d3ae13aa9c77f9
Dominic Meiser <dmeiser79@gmail.com>
2017-07-22 14:26:37 -0600

    Turn ensembles into Monoids

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,5 @@
 module Lib
     ( someFunc
-    , emptyEnsemble
     ) where
 
 import qualified Data.Array.Repa as R
@@ -11,24 +10,39 @@ someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 
-newtype Positions = Positions (R.Array R.U R.DIM2 Double)
-    deriving Show
+type PhaseSpaceShape = R.DIM2
+type PhaseSpaceArray = R.Array R.U PhaseSpaceShape Double
 
-newtype Velocities = Velocities (R.Array R.U R.DIM2 Double)
+newtype PhaseSpace = PhaseSpace {array :: PhaseSpaceArray}
     deriving Show
 
 newtype ParameterName = ParameterName String
     deriving Show
-data ParameterValue = EnsembleProperty (R.Array R.U R.DIM1 Double)
+data ParameterValue = EnsembleProperty (R.Array R.U R.DIM2 Double)
                     | ParticleProperty Double
     deriving Show
 newtype Parameters = Parameters (Map.Map ParameterName ParameterValue)
     deriving Show
 
-data Ensemble = Ensemble Positions Velocities Parameters
+data Ensemble = Ensemble PhaseSpace
               | EmptyEnsemble
     deriving Show
 
-emptyEnsemble = EmptyEnsemble
 
+instance Monoid Ensemble where
+        mempty = EmptyEnsemble
+        mappend EmptyEnsemble b = b
+        mappend a EmptyEnsemble = a
+        mappend (Ensemble ph0) (Ensemble ph1) = Ensemble (PhaseSpace ph)
+          where
+            ph :: PhaseSpaceArray
+            ph = R.computeUnboxedS $
+                 R.reshape (R.Z R.:. (6::Int) R.:. newSize) mergedRows
+            newSize :: Int
+            newSize = ((R.size $ R.extent $ array ph0) + (R.size $ R.extent $ array ph1)) `quot` 6
+            mergedRows = foldr R.append emptyArray rows
+            rows = [R.slice phaseSpaceArray (R.Any R.:. i R.:. R.All)
+                   | phaseSpaceArray <- [array ph0, array ph1]
+                   , i <- [0..(5::Int)]]
+            emptyArray = R.delay $ R.fromListUnboxed (R.Z R.:. (0::Int)) []
 
